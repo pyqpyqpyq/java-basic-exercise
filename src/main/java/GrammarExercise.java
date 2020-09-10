3af27db6fc0b5415e06bd4acb1a7c1b5d78e5b5b
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-10 10:56:25 +0800

    [pyqpyqpyq] feat:javascript-basic_exercise3

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,10 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        String firstWordList = "apple,juice,mother,people,beautiful,apple,dog";
+        String secondWordList = "cat,baby,smile,good,apple,beautiful,Dog,nice";
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
@@ -12,7 +16,14 @@ public class GrammarExercise {
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+
+        String[] List1 =firstWordList.split(",");
+        String[] List2 =secondWordList.split(",");
+        List<String> intersection = Arrays.stream(List1).map(String::toUpperCase).filter(item -> Arrays.stream(List2).map(String::toUpperCase).map(e -> e.toString()).collect(Collectors.toList()).contains(item.toString())).collect(Collectors.toList());
+        List<String> ans=intersection.stream()
+                .distinct()
+                .collect(Collectors.toList());
+//        System.out.println(ans.toString());
+        return ans;
     }
 }

d3c6bf79e282a5773b8aef7f3641a4a091cf867c
liuchanglei <uestclcl@163.com>
2020-09-12 23:23:49 +0800

    完善逻辑

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,4 +1,9 @@
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -6,13 +11,37 @@ public class GrammarExercise {
         String firstWordList = "";
         String secondWordList = "";
 
-        List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
+        List<String> result = findCommonWordsWithSpace(firstWordList, secondWordList);
         //按要求输出到命令行
 
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        Supplier<Stream<String>> supplier1 = () -> Stream.of(firstWordList.toUpperCase().split(","));
+        Supplier<Stream<String>> supplier2 = () -> Stream.of(secondWordList.toUpperCase().split(","));
+
+        checkValid(supplier1);
+        checkValid(supplier2);
+
+        List<String> result = supplier1.get()
+                .filter(e -> supplier2.get().collect(Collectors.toList()).contains(e))
+                .distinct()
+                .sorted(Comparator.comparingInt(o -> o.charAt(0)))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    private static void checkValid(Supplier<Stream<String>> supplier) {
+        final String regs = "^[a-zA-Z]+$";
+        boolean isValid = supplier.get().allMatch(e -> e.matches(regs));
+        if (!isValid) {
+            throw new RuntimeException("input not valid");
+        }
+
+        if (supplier.get().anyMatch(String::isEmpty)) {
+            throw new RuntimeException("input not valid");
+        }
     }
 }
